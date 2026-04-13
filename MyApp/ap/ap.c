8e5a62319536763f63582f8aa1afbb7ea56a107c
kihan <kihan@kcci601>
2026-04-13 15:24:24 +0900

    cli 기본구조

--- MyApp/ap/ap.c
+++ MyApp/ap/ap.c
@@ -2,6 +2,7 @@
 #include "hw_def.h"
 #include "stm32f4xx_hal.h"
 #include "uart.h"
+#include "cli.h"
 #include <stdint.h>
 
 
@@ -12,11 +13,7 @@ void apMain(void){
 
     uartPrintf(0, "Hello World!\r\n");
     while(1){
-        
-        if(uartAvailable(0)>0){
-            uint8_t ch = uartRead(0);
-
-            uartPrintf(0,"%c", ch);
-        }
+        cliMain();
     }
+    
 }
